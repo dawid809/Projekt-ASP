6709b73dd92f4bd34ce7e84770077cc64f75d684
dawid.dziad <dawid.dziad@PK2-01>
2021-10-12 14:56:38 +0200

    Dodaj pliki projektów.

---
+++ Controllers/BookController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projekt_ASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekt_ASP.Controllers
+{
+    public class BookController : Controller
+    {
+        static List<Book> booksList = new List<Book>();
+   
+            
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Book()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult AddNewBook(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                booksList.Add(book);
+                return View("BookList", booksList);
+            }
+            else
+            {
+                return View("Add");
+            }
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekt_ASP.Models
+{
+    public class Book
+    {   
+        [Required(ErrorMessage = "Podaj nazwe książki!")]
+        [DisplayName ("Nazwa książki")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Podaj autora książki!")]
+        [DisplayName ("Autor")]
+        public string Author { get; set; }
+        [Required(ErrorMessage = "Podaj liczbe stron!")]
+        [DisplayName ("Liczba stron")]
+        public int PageCount { get; set; }
+        public DateTime BookDate { get; set; }
+    }
+}

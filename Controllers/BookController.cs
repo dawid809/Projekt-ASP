611e052caf9b3572e7133f38d1fc81fedbcbca16
Dawid Dziad <dawid.dziad@microsoft.wsei.edu.pl>
2021-11-09 14:49:54 +0100

    EF connection done

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -9,62 +9,66 @@ namespace Projekt_ASP.Controllers
 {
     public class BookController : Controller
     {
+
+        private IBookRepository repository;
+        public BookController(IBookRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        //public ViewResult Indexx()
+        //{
+        //    return View(repository.Books);
+        //}
+
         private static int Counter;
 
         private static List<Book> booksList = new List<Book>() 
           {
-              new Book()
-              {
-                  Id = 1, Author = "Bolesław Prus", ReleaseDate = 2015 , Name = "Lalka", PageCount = 864
-              },
-              new Book()
-              {
-                  Id = 2, Author = "Jan Brzechwa", ReleaseDate = 2013 , Name = "Akademia Pana Kleksa", PageCount = 136
-              },
+              //new Book()
+              //{
+              //    Id = 1, Author = "Bolesław Prus", ReleaseDate = 2015 , Name = "Lalka", PageCount = 864
+              //},
+              //new Book()
+              //{
+              //    Id = 2, Author = "Jan Brzechwa", ReleaseDate = 2013 , Name = "Akademia Pana Kleksa", PageCount = 136
+              //},
           };
    
             
         public IActionResult Index()
         {
-            return View();
+            return View(repository.Books);
         }
 
-        public IActionResult Add()
-        {
-            return View();
-        }
+        //public IActionResult Add()
+        //{
+        //    return View();
+        //}
 
-        public IActionResult AddNewBook(Book book)
-        {
-            if (ModelState.IsValid)
-            {
-                booksList.Add(book);
-                Counter++;
-                return View("BookList", booksList);
-            }
-            else
-            {
-                return View("Add");
-            }
-        }
+        //public IActionResult AddNewBook(Book book)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        booksList.Add(book);
+        //        Counter++;
+        //        return View("BookList", booksList);
+        //    }
+        //    else
+        //    {
+        //        return View("Add");
+        //    }
+        //}
 
-        public IActionResult Edit(int id)
-        {
-            //var tmp = booksList.FirstOrDefault(x => x.Id == id);
-            var newBook = booksList.Where(x => x.Id == id).FirstOrDefault();
-            return View(newBook);
-        }
+        //public IActionResult Edit(int id)
+        //{
+        //    //var tmp = booksList.FirstOrDefault(x => x.Id == id);
+        //    var newBook = booksList.Where(x => x.Id == id).FirstOrDefault();
+        //    return View(newBook);
+        //}
 
-        [HttpPost]
-        public IActionResult Edit(Book newBook)
-        {
-            var book = booksList.Where(x => x.Id == newBook.Id).FirstOrDefault();
-            booksList.Remove(book);
-            booksList.Add(newBook);
-            return View("BookList", booksList);
-        }
-
-        //public IActionResult Save(Book newBook)
+        //[HttpPost]
+        //public IActionResult Edit(Book newBook)
         //{
         //    var book = booksList.Where(x => x.Id == newBook.Id).FirstOrDefault();
         //    booksList.Remove(book);
@@ -72,22 +76,30 @@ namespace Projekt_ASP.Controllers
         //    return View("BookList", booksList);
         //}
 
-        public IActionResult Description(int id)
-        {
-            var tmp = booksList.FirstOrDefault(x => x.Id == id);
-            return View(tmp);
-        }
+        ////public IActionResult Save(Book newBook)
+        ////{
+        ////    var book = booksList.Where(x => x.Id == newBook.Id).FirstOrDefault();
+        ////    booksList.Remove(book);
+        ////    booksList.Add(newBook);
+        ////    return View("BookList", booksList);
+        ////}
 
-        public IActionResult Delete(int id)
-        {
-            booksList.Remove(booksList.FirstOrDefault(x => x.Id == id));
-            
-            return View("BookList", booksList);
-        }
+        //public IActionResult Description(int id)
+        //{
+        //    var tmp = booksList.FirstOrDefault(x => x.Id == id);
+        //    return View(tmp);
+        //}
+
+        //public IActionResult Delete(int id)
+        //{
+        //    booksList.Remove(booksList.FirstOrDefault(x => x.Id == id));
+
+        //    return View("BookList", booksList);
+        //}
 
         public IActionResult BookList()
         {
-            return View("BookList", booksList);
+            return View(repository.Books);
         }
     }
 }
---
+++ Models/BooksShopContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Projekt_ASP.Models
+{
+    public interface IBookRepository
+    {
+        IQueryable<Book> Books { get; }
+    }
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+
+    public class EFBookRepository: IBookRepository
+    {
+        private ApplicationDbContext context;
+
+        public EFBookRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IQueryable<Book> Books => context.Books;
+    }
+
+    //public class BooksShopContext : DbContext
+    //{
+    //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //    {
+    //        optionsBuilder.UseSqlServer();
+    //    }
+    //}
+}
